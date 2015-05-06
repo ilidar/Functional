349a1700c40080a7ccf10e21074dd1c1e438028d
Denys Kotelovych <denys.kotelovych@gmail.com>
2015-05-07 01:32:11 +0300

    Added public headers;

--- Functional/Functional.h
+++ Functional/Functional.h
@@ -9,12 +9,9 @@
 
 #import <UIKit/UIKit.h>
 
-
-//! Project version number for Functional.
 FOUNDATION_EXPORT double FunctionalVersionNumber;
-
-//! Project version string for Functional.
 FOUNDATION_EXPORT const unsigned char FunctionalVersionString[];
 
-// In this header, you should import all the public headers of your framework using statements like #import <Functional/PublicHeader.h>
-
+#ifdef __OBJC__
+  #import "NSArray+Functional.h"
+#endif
